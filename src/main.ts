5799c2d9ae92aa29e5209f734f7a51e28d5f29f4
Matheus Marques <matheuspbmarques@protonmail.com>
2025-04-22 23:27:00 -0300

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('Thera Teste BackEnd')
+  .setDescription('API RESTful para gerenciamento de pedidos de produtos, com foco em boas práticas (SOLID), orgnização de código em classes, e manipulação de banco de dados.')
+  .setVersion('1.0.0')
+  .build()
+
+  const swaggerDocumentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDocumentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
