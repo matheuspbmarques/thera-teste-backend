b7b082c7d9e2e7f257b00785afe5a2a14ccda0a0
Matheus Marques <matheuspbmarques@protonmail.com>
2025-04-23 00:02:24 -0300

    add: documentation

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
-import { Prisma } from "generated/prisma";
+import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
+import { Prisma, Products } from "generated/prisma";
 import { CreateProductDto } from "src/dto/products/create.dto";
 import { UpdateProductByIdDto } from "src/dto/products/updateById.dto";
 import { ProductsService } from "src/services/products.service";
@@ -10,6 +11,32 @@ export class ProductsController {
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
+    @ApiOperation({ summary: 'Criar um novo produto' })
+    @ApiBody({
+        schema: {
+            example: {
+                name: 'Nome do Produto',
+                description: 'Descrição do Produto',
+                category: 'Categoria do Produto',
+                price: 100,
+                stock_quantity: 1000
+            } as CreateProductDto
+        }
+    })
+    @ApiResponse({
+        schema: {
+            example: {
+                id: 1,
+                name: 'Nome do Produto Criado',
+                description: 'Descrição do produto criado',
+                category: 'Categoria do Produto Criado',
+                price: 100,
+                stock_quantity: 1000,
+                create_at: new Date(),
+                update_at: new Date(),
+            } as Products
+        }
+    })
     async create(@Body() productData: CreateProductDto) {
         return await this.productsService.create(productData);
     };
