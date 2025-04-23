e93ac12fd55dbf239cca9a63769b03cb7f06d5c8
Matheus Marques <matheuspbmarques@protonmail.com>
2025-04-23 00:20:54 -0300

    add: documentation

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { ApiBody, ApiOperation, ApiResponse } from "@nestjs/swagger";
-import { Prisma, Products } from "generated/prisma";
+import { Products } from "generated/prisma";
 import { CreateProductDto } from "src/dto/products/create.dto";
 import { UpdateProductByIdDto } from "src/dto/products/updateById.dto";
 import { ProductsService } from "src/services/products.service";
@@ -35,18 +35,76 @@ export class ProductsController {
                 create_at: new Date(),
                 update_at: new Date(),
             } as Products
-        }
+        },
+        status: 201
     })
     async create(@Body() productData: CreateProductDto) {
         return await this.productsService.create(productData);
     };
 
     @Get()
+    @ApiOperation({ summary: 'Listar todos os produtos' })
+    @ApiResponse({
+        schema: {
+            example: [
+                {
+                    id: 1,
+                    name: 'Produto 1',
+                    description: 'Descrição do produto 1',
+                    category: 'Categoria do produto 1',
+                    price: 100,
+                    stock_quantity: 1000,
+                    create_at: new Date(),
+                    update_at: new Date()
+                },
+                {
+                    id: 2,
+                    name: 'Produto 2',
+                    description: 'Descrição do produto 2',
+                    category: 'Categoria do produto 2',
+                    price: 50,
+                    stock_quantity: 500,
+                    create_at: new Date(),
+                    update_at: new Date()
+                },
+            ] as Array<Products>
+        },
+        status: 200
+    })
     async getAll() {
         return await this.productsService.getAll();
     };
 
     @Put('/:id')
+    @ApiOperation({
+        summary: 'Atualiza um produto existente'
+    })
+    @ApiBody({
+        schema: {
+            example: {
+                name: 'Novo Nome do Produto',
+                description: 'Descrição nova para o produto',
+                category: 'Nova Categoria do Produto',
+                price: 200,
+                stock_quantity: 2000
+            } as UpdateProductByIdDto,
+        }
+    })
+    @ApiResponse({
+        status: 200,
+        schema: {
+            example: {
+                id: 1,
+                name: 'Novo Nome do Produto',
+                description: 'Descrição nova para o produto',
+                category: 'Nova Categoria do Produto',
+                price: 200,
+                stock_quantity: 2000,
+                create_at: new Date(),
+                update_at: new Date(),
+            } as Products
+        }
+    })
     async update(@Param('id', ParseIntPipe) id: number, @Body() productData: UpdateProductByIdDto) {
         return await this.productsService.updateById(id, productData);
     };
