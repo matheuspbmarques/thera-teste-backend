4e4e734a84477bfc742107090588d8dd588ba14f
Matheus Marques <matheuspbmarques@protonmail.com>
2025-04-23 00:27:30 -0300

    add: documentation

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { Products } from "generated/prisma";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import { ApiBody, ApiResponse } from "@nestjs/swagger";
+import { Orders, Products } from "generated/prisma";
 import { CreateOrderDto } from "src/dto/orders/create.dto";
 import { OrdersService } from "src/services/orders.service";
 
@@ -10,6 +11,31 @@ export class OrdersController {
     ) { };
 
     @Post()
+    @HttpCode(HttpStatus.CREATED)
+    @ApiBody({
+        schema: {
+            example: {
+                products: [
+                    {
+                        id: 1,
+                        quantity: 2
+                    }
+                ]
+            } as CreateOrderDto
+        }
+    })
+    @ApiResponse({
+        schema: {
+            example: {
+                id: 1,
+                total: 1000,
+                create_at: new Date(),
+                update_at: new Date(),
+                status: 'Pendente'
+            } as Orders
+        },
+        status: HttpStatus.CREATED
+    })
     async create(@Body() productsOrder: CreateOrderDto) {
         this.orderService.create(productsOrder.products)
     }
